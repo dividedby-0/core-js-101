063b524c06781e8e5658a7f8a034069f4589cea1
Alessandro <a.ghio86@gmail.com>
2023-01-14 12:37:19 +0100

    feat: add func toCsvText

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -194,8 +194,9 @@ function getTail(arr, n) {
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
-function toCsvText(/* arr */) {
-  throw new Error('Not implemented');
+function toCsvText(arr) {
+  return arr.map((number) => number.join(',')).join('\n');
+  // throw new Error('Not implemented');
 }
 
 /**
