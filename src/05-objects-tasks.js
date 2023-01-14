62f3e6ee65f9d86bbe07f8aa4d217771d7923926
Alessandro <a.ghio86@gmail.com>
2023-01-14 17:34:39 +0100

    feat: add func getJSON

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -28,7 +28,6 @@ function Rectangle(width, height) {
     },
   };
   return rectangle;
-  // throw new Error('Not implemented');
 }
 
 /**
@@ -41,8 +40,8 @@ function Rectangle(width, height) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
